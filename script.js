78986b98c5f74d7851880ba2925f6e17a782e741
Allure <samira-h@hotmail.co.uk>
2022-02-11 22:17:02 +0000

    targeting video in html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Targeting html elements by id and class
 const locationInputEl = document.getElementById("locationInput");
 const button = document.getElementById("submit");
+let video = document.querySelector("video");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "1BjUoXQCNeA20BPARaQHG2ezCoamZAwe";
 const openWeatherAPIKey = "1ca21b13300483dc1e57d37215dcac93";
