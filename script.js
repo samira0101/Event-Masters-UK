709f8e25100086ab0c9f4a60b7a42cb670d81f89
Allure <samira-h@hotmail.co.uk>
2022-02-11 21:59:52 +0000

    Targeting html elements by id and class

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Targeting html elements by id and class
+const locationInputEl = document.getElementById("locationInput");
+const button = document.getElementById("submit");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "1BjUoXQCNeA20BPARaQHG2ezCoamZAwe";
 const openWeatherAPIKey = "1ca21b13300483dc1e57d37215dcac93";
