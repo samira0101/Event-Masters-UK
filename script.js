e800aec00d5896b215a91b30ea0e715bd167bb58
Allure <samira-h@hotmail.co.uk>
2022-02-11 22:14:20 +0000

    Function to be excuted once clicked

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ const button = document.getElementById("submit");
 const ticketMasterAPIKey = "1BjUoXQCNeA20BPARaQHG2ezCoamZAwe";
 const openWeatherAPIKey = "1ca21b13300483dc1e57d37215dcac93";
 
+// Function to get events from ticket masters API, and then dynamically displaying data using cards.
+function searchHandler(e) {
 // Search button event listener to run the function searchHnadler
 button.addEventListener("click", searchHandler)
