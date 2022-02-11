dca6e288809f04f1fd40ae6f44617750b7ecfde4
Allure <samira-h@hotmail.co.uk>
2022-02-11 22:02:49 +0000

    Added event listener button

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const button = document.getElementById("submit");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "1BjUoXQCNeA20BPARaQHG2ezCoamZAwe";
 const openWeatherAPIKey = "1ca21b13300483dc1e57d37215dcac93";
+
+// Search button event listener to run the function searchHnadler
+button.addEventListener("click", searchHandler)
